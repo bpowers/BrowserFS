059eb3fef838dd8551c7b18c117b8c04190386ff
Bobby Powers <bobbypowers@gmail.com>
2015-10-29 14:56:26 -0400

    byte2str from browserify

--- src/core/string_util.ts
+++ src/core/string_util.ts
@@ -163,34 +163,76 @@ export class UTF8 {
   }
 
   public static byte2str(buff: NodeBuffer): string {
-    var chars: number[] = [];
-    var i = 0;
-    while (i < buff.length) {
-      var code = buff.readUInt8(i++);
-      if (code < 0x80) {
-        chars.push(code);
-      } else if (code < 0xC0) {
-        // This is the second byte of a multibyte character. This shouldn't be
-        // possible.
-        throw new Error('Found incomplete part of character in string.');
-      } else if (code < 0xE0) {
-        // 2 bytes: 5 and 6 bits
-        chars.push(((code & 0x1F) << 6) | (buff.readUInt8(i++) & 0x3F));
-      } else if (code < 0xF0) {
-        // 3 bytes: 4, 6, and 6 bits
-        chars.push(((code & 0xF) << 12) | ((buff.readUInt8(i++) & 0x3F) << 6) | (buff.readUInt8(i++) & 0x3F));
-      } else if (code < 0xF8) {
-        // 4 bytes: 3, 6, 6, 6 bits; surrogate pairs time!
-        // First 11 bits; remove 11th bit as per UTF-16 standard
-        var byte3 = buff.readUInt8(i + 2);
-        chars.push(((((code & 0x7) << 8) | ((buff.readUInt8(i++) & 0x3F) << 2) | ((buff.readUInt8(i++) & 0x3F) >> 4)) & 0x3FF) | 0xD800);
-        // Final 10 bits
-        chars.push((((byte3 & 0xF) << 6) | (buff.readUInt8(i++) & 0x3F)) | 0xDC00);
-      } else {
-        throw new Error('Unable to represent UTF-8 string as UTF-16 JavaScript string.');
-      }
-    }
-    return fromCharCodes(chars);
+	let end = buf.length;
+	let res: any[] = [];
+
+	let i = start;
+	while (i < end) {
+		let firstByte = buf[i];
+		let codePoint: any = null;
+		let bytesPerSequence = (firstByte > 0xEF) ? 4
+			: (firstByte > 0xDF) ? 3
+			: (firstByte > 0xBF) ? 2
+			: 1;
+
+		if (i + bytesPerSequence <= end) {
+			let secondByte: any, thirdByte: any, fourthByte: any, tempCodePoint: any;
+
+			switch (bytesPerSequence) {
+			case 1:
+				if (firstByte < 0x80) {
+					codePoint = firstByte;
+				}
+				break;
+			case 2:
+				secondByte = buf[i + 1];
+				if ((secondByte & 0xC0) === 0x80) {
+					tempCodePoint = (firstByte & 0x1F) << 0x6 | (secondByte & 0x3F);
+					if (tempCodePoint > 0x7F) {
+						codePoint = tempCodePoint;
+					}
+				}
+				break;
+			case 3:
+				secondByte = buf[i + 1];
+				thirdByte = buf[i + 2];
+				if ((secondByte & 0xC0) === 0x80 && (thirdByte & 0xC0) === 0x80) {
+					tempCodePoint = (firstByte & 0xF) << 0xC | (secondByte & 0x3F) << 0x6 | (thirdByte & 0x3F);
+					if (tempCodePoint > 0x7FF && (tempCodePoint < 0xD800 || tempCodePoint > 0xDFFF)) {
+						codePoint = tempCodePoint;
+					}
+				}
+				break;
+			case 4:
+				secondByte = buf[i + 1];
+				thirdByte = buf[i + 2];
+				fourthByte = buf[i + 3];
+				if ((secondByte & 0xC0) === 0x80 && (thirdByte & 0xC0) === 0x80 && (fourthByte & 0xC0) === 0x80) {
+					tempCodePoint = (firstByte & 0xF) << 0x12 | (secondByte & 0x3F) << 0xC | (thirdByte & 0x3F) << 0x6 | (fourthByte & 0x3F);
+					if (tempCodePoint > 0xFFFF && tempCodePoint < 0x110000) {
+						codePoint = tempCodePoint;
+					}
+				}
+			}
+		}
+
+		if (codePoint === null) {
+			// we did not generate a valid codePoint so insert a
+			// replacement char (U+FFFD) and advance only 1 byte
+			codePoint = 0xFFFD;
+			bytesPerSequence = 1;
+		} else if (codePoint > 0xFFFF) {
+			// encode to utf16 (surrogate pair dance)
+			codePoint -= 0x10000;
+			res.push(codePoint >>> 10 & 0x3FF | 0xD800);
+			codePoint = 0xDC00 | codePoint & 0x3FF;
+		}
+
+		res.push(codePoint);
+		i += bytesPerSequence;
+	}
+
+	return decodeCodePointsArray(res);
   }
 
   // From http://stackoverflow.com/a/23329386
